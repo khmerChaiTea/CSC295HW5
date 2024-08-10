c46b35552c99086b6e71064692202de6df721c10
pchai <prempreychhay@gmail.com>
2024-08-09 17:53:11 -0700

    Add project files.

---
+++ CSC295HW5/ParenthesisChecker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC295HW5
+{
+    public class ParenthesisChecker
+    {
+        public static bool isBalanced(string expression)
+        {
+            Stack stack = new Stack();
+
+            foreach (char c in expression)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' || c == '}' || c == ']')
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;   // No mathing opening bracket
+                    }
+
+                    char last = (char)stack.Pop();
+
+                    if ((c == ')' && last != '(') || (c == '}' && last != '{') || (c == ']' && last != '['))
+                    {
+                        return false; // Mismatched parentheses
+                    }
+                }
+            }
+
+            return stack.Count == 0; // True if no unmatched opening brackets
+        }
+    }
+}
---
+++ CSC295HW5Tests/ParenthesisCheckerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSC295HW5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC295HW5.Tests
+{
+    [TestClass()]
+    public class ParenthesisCheckerTests
+    {
+        [TestMethod]
+        public void TestSingleTypeBalanced()
+        {
+            Assert.IsTrue(ParenthesisChecker.isBalanced("()"));
+        }
+
+        [TestMethod]
+        public void TestMixedTypeBalanced()
+        {
+            Assert.IsTrue(ParenthesisChecker.isBalanced("{[()]}"));
+        }
+
+        [TestMethod]
+        public void TestUnbalanced()
+        {
+            Assert.IsFalse(ParenthesisChecker.isBalanced("{[()]}["));
+        }
+
+        [TestMethod]
+        public void TestEmptyString()
+        {
+            Assert.IsTrue(ParenthesisChecker.isBalanced(""));
+        }
+
+        [TestMethod]
+        public void TestSingleTypeUnbalanced()
+        {
+            Assert.IsFalse(ParenthesisChecker.isBalanced("(()"));
+        }
+    }
+}
